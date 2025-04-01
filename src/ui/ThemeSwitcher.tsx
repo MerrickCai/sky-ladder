32d9d1790f546e491d885f7501296e2313a90cf2
Merrick Cai <merrickcai.dev@gmail.com>
2025-04-01 14:41:53 -0400

    Refactor ThemeSwitcher component to improve theme management; streamline theme application logic and enhance mounting behavior. Fix flashing issue on theme switch.

--- src/ui/ThemeSwitcher.tsx
+++ src/ui/ThemeSwitcher.tsx
@@ -1,38 +1,44 @@
 "use client";
 
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect } from "react";
 
 const commonClassNames = "cursor-pointer px-2 py-1 text-sm transition-colors rounded";
 const activeClassNames = "bg-gray-900 text-white dark:bg-white dark:text-black";
 const notActiveClassNames =
   "bg-white text-gray-800 dark:bg-gray-800 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700";
 
+type Theme = "light" | "dark" | "system";
+
 export default function ThemeSwitcher() {
-  const [theme, setTheme] = useState<string>("");
+  const [theme, setTheme] = useState<Theme>("system");
+  const [hasMounted, setHasMounted] = useState(false);
 
-  useEffect(() => {
-    const storedTheme = localStorage.getItem("theme") || "system";
-    setTheme(storedTheme);
-  }, []);
+  const removeTheme = () => {
+    setTheme("system");
+    localStorage.removeItem("theme");
+    document.documentElement.classList.remove("dark");
+  };
 
-  function applyTheme(theme: string) {
+  const themeScript = () => {
+    const storedTheme = localStorage.getItem("theme");
+    const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    const isDark = storedTheme === "dark" || (storedTheme === null && prefersDark);
+    document.documentElement.classList.toggle("dark", isDark);
+  };
+
+  function applyTheme(theme: Theme) {
     setTheme(theme);
     localStorage.setItem("theme", theme);
     themeScript();
   }
 
-  const removeTheme = useCallback(() => {
-    setTheme("system");
-    localStorage.removeItem("theme");
-    document.documentElement.classList.remove("dark");
+  useEffect(() => {
+    const storedTheme = (localStorage.getItem("theme") as Theme) || "system";
+    setTheme(storedTheme);
+    setHasMounted(true);
   }, []);
 
-  const themeScript = useCallback(() => {
-    const theme = localStorage.getItem("theme");
-    const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-    const isDark = theme === "dark" || (theme === null && prefersDark);
-    document.documentElement.classList.toggle("dark", isDark);
-  }, []);
+  if (!hasMounted) return null;
 
   return (
     <div className="flex flex-row rounded overflow-hidden border border-gray-300 dark:border-gray-600">
