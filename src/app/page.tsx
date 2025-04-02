8952edc13d9125c739fd50a0192d56441467eb9a
Merrick Cai <merrickcai.dev@gmail.com>
2025-04-01 21:34:20 -0400

    Refactor layout and styling for About and Home pages; unify section styles and improve accessibility in Footer and NavItem components.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,51 @@
 import Link from "next/link";
 
+const commonSessionClassName =
+  "p-8 border rounded-xl shadow-lg bg-white dark:bg-gray-800 border-gray-200 dark:border-gray-700";
+
+const commonTitleClassName = "text-3xl font-semibold text-gray-900 dark:text-gray-100 mb-4";
+
 export default async function Home() {
   return (
-    <div className="flex flex-col h-auto w-full flex-grow items-center justify-center px-4 bg-gradient-to-b from-gray-100 via-gray-200 to-gray-300 dark:from-gray-900 dark:via-gray-800 dark:to-gray-700">
+    <div className="flex flex-col h-auto w-full flex-grow items-center justify-center px-4 py-4 bg-gradient-to-b from-gray-100 via-gray-200 to-gray-300 dark:from-gray-900 dark:via-gray-800 dark:to-gray-700">
       <h1 className="text-5xl font-extrabold text-gray-800 dark:text-gray-100 mb-8 text-center">
         Welcome to My Blog
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl">
-        <section className="p-8 border rounded-xl shadow-lg bg-white dark:bg-gray-800 border-gray-200 dark:border-gray-700">
-          <h2 className="text-3xl font-semibold text-gray-900 dark:text-gray-100 mb-4">About Me</h2>
+        {/* section: About Me */}
+        <section className={commonSessionClassName}>
+          <h2 className={commonTitleClassName}>About Me</h2>
           <p className="text-gray-700 dark:text-gray-300 leading-relaxed">
-            I&apos;m a passionate developer with a love for learning and sharing knowledge. In this
-            blog, you will find articles on various topics including programming, technology trends,
-            and personal development.
+            {`I'm a passionate developer with a love for learning and sharing knowledge. In this blog,
+            you will find articles on various topics including programming, technology trends, and
+            personal development.`}
           </p>
         </section>
-        <section className="p-8 border rounded-xl shadow-lg bg-white dark:bg-gray-800 border-gray-200 dark:border-gray-700">
-          <h2 className="text-3xl font-semibold text-gray-900 dark:text-gray-100 mb-4">
-            My Projects
-          </h2>
+
+        {/* section: My Projects */}
+        <section className={commonSessionClassName}>
+          <h2 className={commonTitleClassName}>My Projects</h2>
           <p className="text-gray-700 dark:text-gray-300 leading-relaxed">
-            I enjoy working on a variety of projects, from web development to open-source
-            contributions. Check out my latest projects and see what I&apos;m currently working on.
+            {`I enjoy working on a variety of projects, from web development to open-source
+            contributions. Check out my latest projects and see what I'm currently working on.`}
           </p>
         </section>
-        <section className="p-8 border rounded-xl shadow-lg bg-white dark:bg-gray-800 border-gray-200 dark:border-gray-700">
-          <h2 className="text-3xl font-semibold text-gray-900 dark:text-gray-100 mb-4">Contact</h2>
+
+        {/* section: Contact */}
+        <section className={commonSessionClassName}>
+          <h2 className={commonTitleClassName}>Contact</h2>
           <p className="text-gray-700 dark:text-gray-300 leading-relaxed">
-            Feel free to reach out to me through the contact page. I&apos;m always open to
-            discussing new ideas, collaborations, or just having a chat about technology.
+            {`Feel free to reach out to me through the contact page. I'm always open to discussing new
+            ideas, collaborations, or just having a chat about technology.`}
           </p>
         </section>
-        <section className="p-8 border rounded-xl shadow-lg bg-white dark:bg-gray-800 border-gray-200 dark:border-gray-700">
-          <h2 className="text-3xl font-semibold text-gray-900 dark:text-gray-100 mb-4">
-            Frontend Development Notes
-          </h2>
+
+        {/* section: Frontend Development Notes */}
+        <section className={commonSessionClassName}>
+          <h2 className={commonTitleClassName}>Frontend Development Notes</h2>
           <p className="text-gray-700 dark:text-gray-300 leading-relaxed">
-            Explore my detailed notes on frontend development, where I document my learnings and
-            best practices.
+            {`Explore my detailed notes on frontend development, where I document my learnings and
+            best practices.`}
           </p>
           <Link
             href="https://merrickcai.notion.site/Frontend-Development-188e3a07cc988036a524c65fb43d2d06"
@@ -48,15 +56,15 @@ export default async function Home() {
             View Notes on Notion
           </Link>
         </section>
-        <section className="p-8 border rounded-xl shadow-lg bg-white dark:bg-gray-800 border-gray-200 dark:border-gray-700">
-          <h2 className="text-3xl font-semibold text-gray-900 dark:text-gray-100 mb-4">
-            Upcoming Features
-          </h2>
+
+        {/* section: Upcoming Features */}
+        <section className={commonSessionClassName}>
+          <h2 className={commonTitleClassName}>Upcoming Features</h2>
           <p className="text-gray-700 dark:text-gray-300 leading-relaxed">
-            Exciting updates are on the way! The site will soon be connected to a backend database
+            {`Exciting updates are on the way! The site will soon be connected to a backend database
             hosted on Vercel, enabling dynamic content and enhanced functionality. Additionally,
             support for Markdown and MDX will be introduced, leveraging Next.js's official
-            integration to create rich, flexible content.
+            integration to create rich, flexible content.`}
           </p>
         </section>
       </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,8 @@ import ThemeSwitcher from "@/ui/ThemeSwitcher";
 
 export default async function Header() {
   return (
-    <header className="bg-zinc-50/70 dark:bg-black/70 shadow-none shadow-gray-100 dark:shadow-none backdrop-blur-sm sticky top-0 z-10 w-full h-18 flex flex-row justify-center gap-10 items-center">
-      <nav className="w-auto h-full flex flex-row justify-center gap-3 p-2 items-center">
+    <header className=" bg-zinc-50/70 dark:bg-black/70 shadow-none shadow-gray-100 dark:shadow-none backdrop-blur-sm sticky top-0 z-10 w-full h-14 md:h-18 flex flex-row md:justify-around justify-around gap-0.5 md:gap-10 items-center">
+      <nav className="w-auto h-full flex flex-row justify-center gap-0.5 md:gap-3 md:p-2 items-center">
         {navLinks.map((link) => (
           <NavItem key={link.href} link={link} />
         ))}
